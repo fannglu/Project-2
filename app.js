d76a347b9f91110efc2cadf018f83448fdb45623
Fang Lu Lim <fanglulim96@gmail.com>
2022-04-07 16:19:31 -0700

    git ignore

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
+var passport = require("passport");
+var Strategy = require("passport-local").Strategy;
+var session = require("cookie-session");
+
+var db = require("./db");
+
 const app = express();
 const port = 3000;
 
+app.get('/', function(req, res){
+    
+})
