21270d7901a64226816084216313b770d8c253a4
Fang Lu Lim <fanglulim96@gmail.com>
2022-04-07 23:57:32 -0700

    form

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-require("dotenv").config();
 const express = require("express");
-const mongoose = require("mongoose");
-var passport = require("passport");
-var Strategy = require("passport-local").Strategy;
-var session = require("cookie-session");
-
-var db = require("./db");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
-const port = 3000;
-
-app.get('/', function(req, res){
-    
-})
+const port = 300;
